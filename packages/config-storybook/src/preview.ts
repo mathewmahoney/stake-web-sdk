730917dec33ba408c50b19a2bb513ae4cf292b93
Dylan Qin <dylan@easygo.io>
2025-04-08 16:26:42 +1000

    update storybook settings

--- packages/config-storybook/src/preview.ts
+++ packages/config-storybook/src/preview.ts
@@ -1,11 +1,26 @@
 import type { Preview } from '@storybook/svelte';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 const preview: Preview = {
 	parameters: {
+		layout: 'fullscreen',
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i,
+			},
+		},
+		viewport: {
+			defaultViewport: 'stake',
+			viewports: {
+				...INITIAL_VIEWPORTS,
+				stake: {
+					name: 'stake iframe',
+					styles: {
+						width: '1200px',
+						height: '675px',
+					},
+				},
 			},
 		},
 	},
